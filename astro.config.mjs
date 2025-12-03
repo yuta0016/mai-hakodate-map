93752fd69ea5f1a6e6ddf5c9ff19738184f13a7e
yuta0016 <i.yuta16@gmail.com>
2025-12-03 23:16:38 +0900

    GitHub Pages deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://yuta0016.github.io', // 必ずあなたのユーザー名に置き換えてください
+    base: '/mai-hakodate-map', // 必ずあなたのリポジトリ名に置き換えてください
+});
