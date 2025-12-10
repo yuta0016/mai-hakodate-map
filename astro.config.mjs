e8dd3c82d20b7f59584d52a33c53e96ec1a5c423
yuta0016 <i.yuta16@gmail.com>
2025-12-10 21:15:23 +0900

    delete all

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://yuta0016.github.io',
-    base: '/mai-hakodate-map',
-    output: 'static',
-});
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-export default {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
